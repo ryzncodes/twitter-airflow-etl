05c67f1219eb0edf3d10d5ca87b9cfa5f4d7c3e8
Faiz <faizkasman97@gmail.com>
2023-04-09 17:34:55 +0800

    Extracted Twitter data.

--- twitter_etl.py
+++ twitter_etl.py
@@ -14,4 +14,26 @@ consumer_secret = creds.ACCESS_TOKEN_SECRET
 auth = tweepy.OAuthHandler(access_key,access_secret)
 auth.set_access_token(consumer_key,consumer_secret)
 
+# API Object
+api = tweepy.API(auth)
 
+tweets = api.user_timeline(screen_name = '@AzriWalter',
+                           count = 200,
+                           include_rts = False,
+                           tweet_mode = 'extended' #Extended to keep the full tweet text.
+                           )
+
+tweet_list = []
+for tweet in tweets:
+    text = tweet._json["full_text"]
+
+    refined_tweet = {'user': tweet.user.screen_name,
+                     'text' : text,
+                     'favorite_count': tweet.favorite_count,
+                     'retweet_count' : tweet.retweet_count,
+                     'created_at' : tweet.created_at}
+    
+    tweet_list.append(refined_tweet)
+
+df = pd.DataFrame(tweet_list)
+df.to_csv("azriwalter_twitter_data.csv")
